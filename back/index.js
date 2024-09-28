d96adae6a362f8df5b3951d0538abcffa52b6dc2
Italo Rodrigues Adler <italo.rodrigues@digitalcollegesul.local>
2024-09-27 22:23:39 -0300

    atualiza endpoint de criação de conta verificando se email já existe na base

--- back/index.js
+++ back/index.js
@@ -17,30 +17,43 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/signup', (req, res) => {
-   const { email, pass } = req.body;
+  const { email, pass } = req.body;
 
+  // Verificar se o email já existe
+  const checkEmailQuery = {
+    text: 'SELECT * FROM users WHERE email = $1',
+    values: [email],
+  };
 
-   const hash = crypto.createHash('sha256', pass).digest('hex');
-   
-   const query = {
-     text: 'INSERT INTO users(name, password, created_at) VALUES($1, $2, NOW())',
-     values: [email, hash],
-   };
-   
-
-   pool.query(query, (err, res) => {
+  pool.query(checkEmailQuery, (err, result) => {
     if (err) {
       console.error('Error executing query', err.stack);
-    } else {
-      console.log('User created');
-      if (res && res.rows) {
-        console.log(res.rows);
-      }
+      return res.status(500).json({ message: 'Internal server error' });
     }
+
+    if (result.rows.length > 0) {
+      // Email já existe
+      return res.status(400).json({ message: 'Email already in use' });
+    }
+
+    // Email não existe, prosseguir com a inserção
+    const hash = crypto.createHash('sha256').update(pass).digest('hex');
+
+    const insertUserQuery = {
+      text: 'INSERT INTO users(email, password, created_at) VALUES($1, $2, NOW())',
+      values: [email, hash],
+    };
+
+    pool.query(insertUserQuery, (err, result) => {
+      if (err) {
+        console.error('Error executing query', err.stack);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+
+      console.log('User created');
+      res.status(201).json({ message: 'User created' });
+    });
   });
- 
-  console.log(email, pass, hash);
-  res.status(201).json({ message: 'User created' });
 });
 
 app.post('/login', (req, res) => {
